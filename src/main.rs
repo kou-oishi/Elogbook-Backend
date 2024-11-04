18408d5bd02a82037d4fe79924b56e392a8e2bd0
Kou Oishi <kou.oishi@kek.jp>
2024-11-04 23:25:13 +0900

    Make download URL for security

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,30 @@
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
-use actix_multipart::Multipart;
-use futures::StreamExt;
-use serde::Deserialize;
 use actix_cors::Cors;
+use actix_multipart::Multipart;
+use actix_web::{web, App, Error, HttpRequest, HttpResponse, HttpServer, Responder};
+use chrono::{Datelike, Duration, Utc};
+use futures::StreamExt;
 use mongodb::bson::doc;
-use sha2::{Sha256, Digest}; 
-use chrono::Datelike;
-use std::io::Write;
+use serde::Deserialize;
+use sha2::{Digest, Sha256};
 use std::fs::File;
+use std::io::Write;
+
+use actix_files::NamedFile;
+use lazy_static::lazy_static;
+use rand::{distributions::Alphanumeric, Rng};
+use std::collections::HashMap;
+use tokio::sync::Mutex;
 
 use elogbook::get_db_collection;
-use elogbook::models::{Attachment, Entry, EntryResponse};
+use elogbook::models::*;
 
 #[derive(Deserialize)]
 struct GetEntriesParams {
-    limit:  i64,
+    limit: i64,
     offset: u64,
 }
 
 async fn add_entry(mut payload: Multipart) -> impl Responder {
-
     // Prepare a vacant entry
     let mut entry = Entry {
         id: None,
@@ -29,30 +34,34 @@ async fn add_entry(mut payload: Multipart) -> impl Responder {
     };
     // Empty attachments
     let mut attachments = Vec::new();
-    
+
     while let Some(item) = payload.next().await {
         use std::path::Path;
 
         let mut field = item.expect("Error processing multipart field");
-        
+
         let content_disposition = field.content_disposition().clone();
         let field_name = content_disposition.get_name().unwrap_or_default();
 
         // Content
         if field_name == "content" {
             while let Some(chunk) = field.next().await {
-                entry.content.push_str(std::str::from_utf8(&chunk.expect("Error getting a log content")).unwrap());
+                entry.content.push_str(
+                    std::str::from_utf8(&chunk.expect("Error getting a log content")).unwrap(),
+                );
             }
         }
         // Attachments
         else if field_name == "file" {
             let mime_type = field.content_type().to_string();
             let original_filename = content_disposition.get_filename().unwrap_or("tmpfile");
-            let directory_path = format!("./attachments/{:04}/{:02}/{:02}", 
-                                                    entry.created_at.naive_utc().year(),
-                                                    entry.created_at.naive_utc().month(),
-                                                    entry.created_at.naive_utc().day());
-            
+            let directory_path = format!(
+                "./attachments/{:04}/{:02}/{:02}",
+                entry.created_at.naive_utc().year(),
+                entry.created_at.naive_utc().month(),
+                entry.created_at.naive_utc().day()
+            );
+
             // Make a unique (hashed) name
             let mut hasher = Sha256::new();
             hasher.update(entry.created_at.to_rfc3339().as_bytes());
@@ -66,17 +75,22 @@ async fn add_entry(mut payload: Multipart) -> impl Responder {
                     hashed_filename += &format!(".{}", ext_str);
                 }
             }
-            
+
             std::fs::create_dir_all(&directory_path).expect("Cannot make directory");
-            let mut f = File::create(&hashed_filename).expect(&format!("Error creating file: {}", &hashed_filename));
+            let mut f = File::create(&hashed_filename)
+                .expect(&format!("Error creating file: {}", &hashed_filename));
 
             while let Some(chunk) = field.next().await {
                 let data = match chunk {
                     Ok(d) => d,
-                    Err(e) => return HttpResponse::InternalServerError().body(format!("Error reading data: {}", e)),
+                    Err(e) => {
+                        return HttpResponse::InternalServerError()
+                            .body(format!("Error reading data: {}", e))
+                    }
                 };
                 if let Err(e) = f.write_all(&data) {
-                    return HttpResponse::InternalServerError().body(format!("Error writing to file: {}", e));
+                    return HttpResponse::InternalServerError()
+                        .body(format!("Error writing to file: {}", e));
                 }
             }
 
@@ -95,16 +109,23 @@ async fn add_entry(mut payload: Multipart) -> impl Responder {
     }
 
     // Insert this log entry
-    let collection = get_db_collection().await.expect("Failed to connect to the DB.");
-    collection.insert_one(entry, None).await.expect("Error saving new entry");
+    let collection = get_db_collection()
+        .await
+        .expect("Failed to connect to the DB.");
+    collection
+        .insert_one(entry, None)
+        .await
+        .expect("Error saving new entry");
 
     HttpResponse::Ok().body("Entry added to database.")
 }
 
-async fn get_entries(params: web::Query<GetEntriesParams>) -> impl Responder{
-    use futures_util::stream::TryStreamExt;  // TryStreamExtのインポート
+async fn get_entries(params: web::Query<GetEntriesParams>) -> impl Responder {
+    use futures_util::stream::TryStreamExt;
 
-    let collection = get_db_collection().await.expect("Failed to connect to the DB.");
+    let collection = get_db_collection()
+        .await
+        .expect("Failed to connect to the DB.");
 
     let find_options = mongodb::options::FindOptions::builder()
         .sort(mongodb::bson::doc! {"_id": -1})
@@ -112,29 +133,94 @@ async fn get_entries(params: web::Query<GetEntriesParams>) -> impl Responder{
         .skip(params.offset)
         .build();
 
-    let mut cursor = collection.find(None, find_options).await.expect("Error finding entries.");
+    let mut cursor = collection
+        .find(None, find_options)
+        .await
+        .expect("Error finding entries.");
     let mut results = Vec::new();
 
     while let Some(entry) = cursor.try_next().await.expect("Error parsing entry") {
+        let mut attachments_response = vec![];
+
+        // Process attachments
+        if let Some(attachments) = entry.attachments {
+            for attachment in attachments {
+                // Make download URL
+                let download_url =
+                    generate_temporary_download_url(attachment.saved_path.clone()).await;
+
+                // Don't pass the saved path to the frontend!
+                attachments_response.push(AttachmentResponse {
+                    id: attachment.id,
+                    mime: attachment.mime.clone(),
+                    original_name: attachment.original_name,
+                    download_url: download_url,
+                });
+            }
+        }
+
         let entry_response = EntryResponse {
-            id:         entry.id.map(|oid| oid.to_hex()),  // ObjectIdをStringに変換
-            content:    entry.content,
+            id: entry.id.map(|oid| oid.to_hex()),
+            content: entry.content,
             created_at: entry.created_at,
-            attachments: entry.attachments,
+            attachments: attachments_response,
         };
         results.push(entry_response);
     }
-    
+
     HttpResponse::Ok().json(results)
 }
 
-async fn greet() -> impl Responder{
+lazy_static! {
+    static ref DOWNLOAD_REQUESTS: Mutex<HashMap<String, DownloadRequest>> =
+        Mutex::new(HashMap::new());
+}
+
+// 一時的なダウンロードURLを生成
+async fn generate_temporary_download_url(file_path: String) -> String {
+    let token: String = rand::thread_rng()
+        .sample_iter(&Alphanumeric)
+        .take(30)
+        .map(char::from)
+        .collect();
+
+    let expires_at = Utc::now() + Duration::minutes(1); // available 1min
+
+    let download_request = DownloadRequest {
+        token: token.clone(),
+        file_path,
+        expires_at,
+    };
+
+    DOWNLOAD_REQUESTS
+        .lock()
+        .await
+        .insert(token.clone(), download_request);
+    format!("/download/{}", token)
+}
+
+async fn download_file(req: HttpRequest, path: web::Path<String>) -> Result<HttpResponse, Error> {
+    let token = path.into_inner();
+    let requests = DOWNLOAD_REQUESTS.lock().await;
+
+    if let Some(request) = requests.get(&token) {
+        if Utc::now() < request.expires_at {
+            return Ok(NamedFile::open_async(&request.file_path)
+                .await?
+                .into_response(&req));
+        }
+    }
+
+    Ok(HttpResponse::NotFound().body("Link expired or invalid"))
+}
+
+async fn greet() -> impl Responder {
     HttpResponse::Ok().body("Hello World!")
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    use actix_files as fs;
+    //use actix_files as fs;
 
     HttpServer::new(|| {
         App::new()
@@ -144,11 +230,12 @@ async fn main() -> std::io::Result<()> {
                     .allow_any_method()
                     .allow_any_header()
                     .max_age(3600),
-            ) 
-            .service(fs::Files::new("/attachments", "./attachments").show_files_listing())
+            )
+            //.service(fs::Files::new("/attachments", "./attachments").show_files_listing())
             .route("/", web::get().to(greet))
             .route("/add_entry", web::post().to(add_entry))
             .route("/get_entries", web::get().to(get_entries))
+            .route("/download/{token}", web::get().to(download_file))
     })
     .bind("127.0.0.1:8080")?
     .run()
--- src/models.rs
+++ src/models.rs
@@ -1,12 +1,12 @@
-use serde::{Deserialize, Serialize};
 use chrono::{DateTime, Utc};
 use mongodb::bson::oid::ObjectId;
+use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Attachment {
-    pub id: u32, 
-    pub saved_path: String, 
-    pub original_name: String, 
+    pub id: u32,
+    pub saved_path: String,
+    pub original_name: String,
     pub mime: String,
 }
 pub type Attachments = Vec<Attachment>;
@@ -17,14 +17,31 @@ pub struct Entry {
     pub id: Option<ObjectId>,
     pub content: String,
     pub created_at: chrono::DateTime<Utc>,
-    #[serde(skip_serializing_if = "Option::is_none")]  // attachmentsが空の場合はスキップ
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub attachments: Option<Attachments>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
+pub struct AttachmentResponse {
+    pub id: u32,
+    pub mime: String,
+    pub original_name: String,
+    pub download_url: String,
+}
+pub type AttachmentsResponse = Vec<AttachmentResponse>;
+
+#[derive(Debug, Serialize, Deserialize)]
 pub struct EntryResponse {
-    pub id: Option<String>,  // For the frontend
+    pub id: Option<String>, // For the frontend
     pub content: String,
     pub created_at: DateTime<Utc>,
-    pub attachments: Option<Attachments>,
+    pub attachments: AttachmentsResponse,
+}
+
+// For download files
+#[derive(Debug, Clone)]
+pub struct DownloadRequest {
+    pub token: String,
+    pub file_path: String,
+    pub expires_at: DateTime<Utc>,
 }
